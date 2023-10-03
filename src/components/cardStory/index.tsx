3ff8f5c018a5958ddbce2f7eb898c98c9c619f1a
Otavin23 <alvesotavio132@gmail.com>
2023-10-03 15:08:42 -0300

    :lipstick: style: componentize story cards

---
+++ src/components/cardStory/index.tsx
@@ -0,0 +1,70 @@
+import { Box, Image, Flex, Text, Button, Heading } from "@chakra-ui/react";
+
+interface IProps {
+  title: string;
+  author: string;
+  image: string;
+}
+
+const CardStory = ({ title, author, image }: IProps) => {
+  return (
+    <Box
+      as="div"
+      w="100%"
+      h="450px"
+      pos="relative"
+      _before={{
+        content: `""`,
+        pos: "absolute",
+        bg: "linear-gradient(0deg, rgba(0, 0, 0, 0.66) 0px, transparent);",
+        w: "100%",
+        h: "100%",
+      }}
+    >
+      <Image src={image} alt="" w="100%" h="100%" />
+      <Flex
+        pos="absolute"
+        direction="column"
+        bottom="1px"
+        px="3rem"
+        py="2rem"
+        w="100%"
+      >
+        <Heading as="h3" color="#fffd" fontSize="20px">
+          {title}
+        </Heading>
+        <Text
+          as="span"
+          color="rgba(217, 217, 217, 0.914)"
+          fontWeight="700"
+          my="0rem"
+          py="0.5rem"
+          fontSize="14px"
+          letterSpacing="1px"
+          borderBottom="0.01rem solid rgba(217, 217, 217, 0.44);"
+        >
+          by {author}
+        </Text>
+        <Button
+          mt="0.5rem"
+          bg="transparent"
+          color="#fff"
+          p="0"
+          w="100%"
+          display="flex"
+          justifyContent="space-between"
+          fontSize="14px"
+        >
+          READ STORY
+          <svg xmlns="http://www.w3.org/2000/svg" width="43" height="14">
+            <g fill="none" fill-rule="evenodd" stroke="#ffffff">
+              <path d="M0 7h41.864M35.428 1l6 6-6 6" />
+            </g>
+          </svg>
+        </Button>
+      </Flex>
+    </Box>
+  );
+};
+
+export { CardStory };
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,6 @@
 import { Footer } from "../../components/footer";
 import { Header } from "../../components/header";
+import { CardStory } from "../../components/cardStory";
 import {
   Container,
   Flex,
@@ -229,270 +230,29 @@ const Home = () => {
           </Flex>
 
           <Flex as="section" justifyContent="space-between">
-            <Box
-              as="div"
-              w="100%"
-              h="450px"
-              pos="relative"
-              _before={{
-                content: `""`,
-                pos: "absolute",
-                bg: "linear-gradient(0deg, rgba(0, 0, 0, 0.66) 0px, transparent);",
-                w: "100%",
-                h: "100%",
-              }}
-            >
-              <Image
-                src="../assets/stories/desktop/mountains.jpg"
-                alt=""
-                w="100%"
-                h="100%"
-              />
-              <Flex
-                pos="absolute"
-                direction="column"
-                bottom="1px"
-                px="3rem"
-                py="2rem"
-                w="100%"
-              >
-                <Heading as="h3" color="#fffd" fontSize="20px">
-                  The Mountain
-                </Heading>
-                <Text
-                  as="span"
-                  color="rgba(217, 217, 217, 0.914)"
-                  fontWeight="700"
-                  my="0rem"
-                  py="0.5rem"
-                  fontSize="14px"
-                  letterSpacing="1px"
-                  borderBottom="0.01rem solid rgba(217, 217, 217, 0.44);"
-                >
-                  by John Apppleseed
-                </Text>
-                <Button
-                  mt="0.5rem"
-                  bg="transparent"
-                  color="#fff"
-                  p="0"
-                  w="100%"
-                  display="flex"
-                  justifyContent="space-between"
-                  fontSize="14px"
-                >
-                  READ STORY
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="43"
-                    height="14"
-                  >
-                    <g fill="none" fill-rule="evenodd" stroke="#ffffff">
-                      <path d="M0 7h41.864M35.428 1l6 6-6 6" />
-                    </g>
-                  </svg>
-                </Button>
-              </Flex>
-            </Box>
+            <CardStory
+              title="The Mountain"
+              author="John Appleseed"
+              image="../assets/stories/desktop/mountains.jpg"
+            />
 
-            <Box
-              as="div"
-              w="100%"
-              h="450px"
-              pos="relative"
-              _before={{
-                content: `""`,
-                pos: "absolute",
-                bg: "linear-gradient(0deg, rgba(0, 0, 0, 0.66) 0px, transparent);",
-                w: "100%",
-                h: "100%",
-              }}
-            >
-              <Image
-                src="../assets/stories/desktop/cityscapes.jpg"
-                alt=""
-                w="100%"
-                h="100%"
-              />
+            <CardStory
+              title="Sunset Cityscapes"
+              author="Benjamin Crud"
+              image="../assets/stories/desktop/cityscapes.jpg"
+            />
 
-              <Flex
-                pos="absolute"
-                direction="column"
-                bottom="1px"
-                px="3rem"
-                py="2rem"
-                w="100%"
-              >
-                <Heading as="h3" color="#fffd" fontSize="20px">
-                  Sunset Cityscapes
-                </Heading>
-                <Text
-                  as="span"
-                  color="rgba(217, 217, 217, 0.914)"
-                  fontWeight="700"
-                  my="0rem"
-                  py="0.5rem"
-                  fontSize="14px"
-                  letterSpacing="1px"
-                  borderBottom="0.01rem solid rgba(217, 217, 217, 0.44);"
-                >
-                  by Benjamin Crud
-                </Text>
-                <Button
-                  mt="0.5rem"
-                  bg="transparent"
-                  color="#fff"
-                  p="0"
-                  w="100%"
-                  display="flex"
-                  justifyContent="space-between"
-                  fontSize="14px"
-                >
-                  READ STORY
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="43"
-                    height="14"
-                  >
-                    <g fill="none" fill-rule="evenodd" stroke="#ffffff">
-                      <path d="M0 7h41.864M35.428 1l6 6-6 6" />
-                    </g>
-                  </svg>
-                </Button>
-              </Flex>
-            </Box>
+            <CardStory
+              title="18 Days Voyage"
+              author="Alexei Borodin"
+              image="../assets/stories/desktop/18-days-voyage.jpg"
+            />
 
-            <Box
-              as="div"
-              w="100%"
-              h="450px"
-              pos="relative"
-              _before={{
-                content: `""`,
-                pos: "absolute",
-                bg: "linear-gradient(0deg, rgba(0, 0, 0, 0.66) 0px, transparent);",
-                w: "100%",
-                h: "100%",
-              }}
-            >
-              <Image
-                src="../assets/stories/desktop/18-days-voyage.jpg"
-                alt=""
-                w="100%"
-                h="100%"
-              />
-              <Flex
-                pos="absolute"
-                direction="column"
-                bottom="1px"
-                px="3rem"
-                py="2rem"
-                w="100%"
-              >
-                <Heading as="h3" color="#fffd" fontSize="20px">
-                  18 Days Voyage
-                </Heading>
-                <Text
-                  as="span"
-                  color="rgba(217, 217, 217, 0.914)"
-                  fontWeight="700"
-                  my="0rem"
-                  py="0.5rem"
-                  fontSize="14px"
-                  letterSpacing="1px"
-                  borderBottom="0.01rem solid rgba(217, 217, 217, 0.44);"
-                >
-                  by Alexei Borodin
-                </Text>
-                <Button
-                  mt="0.5rem"
-                  bg="transparent"
-                  color="#fff"
-                  p="0"
-                  w="100%"
-                  display="flex"
-                  justifyContent="space-between"
-                  fontSize="14px"
-                >
-                  READ STORY
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="43"
-                    height="14"
-                  >
-                    <g fill="none" fill-rule="evenodd" stroke="#ffffff">
-                      <path d="M0 7h41.864M35.428 1l6 6-6 6" />
-                    </g>
-                  </svg>
-                </Button>
-              </Flex>
-            </Box>
-
-            <Box
-              as="div"
-              w="100%"
-              h="450px"
-              pos="relative"
-              _before={{
-                content: `""`,
-                pos: "absolute",
-                bg: "linear-gradient(0deg, rgba(0, 0, 0, 0.66) 0px, transparent);",
-                w: "100%",
-                h: "100%",
-              }}
-            >
-              <Image
-                src="../assets/stories/desktop/architecturals.jpg"
-                alt=""
-                w="100%"
-                h="100%"
-              />
-              <Flex
-                pos="absolute"
-                direction="column"
-                bottom="1px"
-                px="3rem"
-                py="2rem"
-                w="100%"
-              >
-                <Heading as="h3" color="#fffd" fontSize="20px">
-                  Architecturals
-                </Heading>
-                <Text
-                  as="span"
-                  color="rgba(217, 217, 217, 0.914)"
-                  fontWeight="700"
-                  my="0rem"
-                  py="0.5rem"
-                  fontSize="14px"
-                  letterSpacing="1px"
-                  borderBottom="0.01rem solid rgba(217, 217, 217, 0.44);"
-                >
-                  by John Apppleseed
-                </Text>
-                <Button
-                  mt="0.5rem"
-                  bg="transparent"
-                  color="#fff"
-                  p="0"
-                  w="100%"
-                  display="flex"
-                  justifyContent="space-between"
-                  fontSize="14px"
-                >
-                  READ STORY
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="43"
-                    height="14"
-                  >
-                    <g fill="none" fill-rule="evenodd" stroke="#ffffff">
-                      <path d="M0 7h41.864M35.428 1l6 6-6 6" />
-                    </g>
-                  </svg>
-                </Button>
-              </Flex>
-            </Box>
+            <CardStory
+              title="Architecturals"
+              author="John Apppleseed"
+              image="../assets/stories/desktop/architecturals.jpg"
+            />
           </Flex>
 
           <Flex as="section" justify="center">
